084ff4548dc7dfaf18a93e718cd8242b8584a36a
YifanXu1999 <yifanxu1999@gmail.com>
2024-10-17 20:48:28 -0400

    reactjs expression

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+const count = 100;
+
+const getName = () => {
+    return 'jack'
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+          {/* 1. string */}
+          {'this is message'}
+          {/* 2. variable */}
+          {count}
+          {/*3. function call*/}
+          {getName()}
+      </div>
+  )
+
 }
 
 export default App;
--- readme.md
+++ readme.md
@@ -10,3 +10,26 @@ Create React APP
 npx create-react-app 
 ```
 
+
+
+# Basic JSX Interpolation
+
+1. String
+2. variable
+3. Function call
+
+```react
+      <div className="App">
+          {/* 1. string */}
+          {'this is message'}
+          {/* 2. variable */}
+          {count}
+          {/*3. function call*/}
+          {getName()}
+      </div>
+```
+
+
+
+# List
+
