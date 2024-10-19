d69c4cdc4ee31ac57ef58e32af00ba087127d6e9
YifanXu1999 <yifanxu1999@gmail.com>
2024-10-19 13:45:57 -0400

    create router

--- react-router-pro/src/index.js
+++ react-router-pro/src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import router from "./router";
+import {RouterProvider} from "react-router-dom";
 
-const router = createBrowserRouter([
-    {
-        path: '/', element: <div>Hello world!</div>
-    },
-    {
-        path: '/about', element: <div>About</div>
-    }
-]);
+
 
 const root = ReactDOM
     .createRoot(document.getElementById('root'))
-    .render(
+root.render(
         <RouterProvider router={router}>
         </RouterProvider>
     )
