2d51e458241decfa6ef5e42dbd0de778a9cd3a30
YifanXu1999 <yifanxu1999@gmail.com>
2024-10-18 23:34:02 -0400

    redux init

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,15 +1,16 @@
 import {useSelector} from "react-redux";
 import {useDispatch} from "react-redux";
-import {decrement, increment} from "./store/modules/counterStore";
+import {decrement, increment, incrementByAmount} from "./store/modules/counterStore";
 
 function App() {
   const {count} = useSelector(state => state.counter)
   const dispatch = useDispatch()
   return (
       <div>
-        {count}
-        <button onClick={() => dispatch(increment())}>+</button>
-        <button onClick={() => dispatch(decrement())}>-</button>
+          {count}
+          <button onClick={() => dispatch(increment())}>+</button>
+          <button onClick={() => dispatch(decrement())}>-</button>
+          <button onClick={() => dispatch(incrementByAmount(20))}>Add 20 </button>
       </div>
   );
 }
