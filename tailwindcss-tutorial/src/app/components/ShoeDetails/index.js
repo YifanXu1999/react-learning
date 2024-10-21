b21036333d3aa2943bc3eddecf2e2cb8d989efd1
YifanXu1999 <yifanxu1999@gmail.com>
2024-10-21 15:28:30 -0400

    Show layout

---
+++ tailwindcss-tutorial/src/app/components/ShoeDetails/index.js
@@ -0,0 +1,33 @@
+import nike1 from "../../assets/n1-min.png"
+import Image from 'next/image'
+export function ShoeDetails () {
+    return (
+        <div className="flex flex-col items-center justify-center md:flex-row-reverse">
+            <div className="flex-1">
+                <Image src={nike1} alt="abc"></Image>
+            </div>
+
+            <div className="flex-1 space-y-6">
+                <div className="text-5xl font-black md:text-6xl">Nike Air Max 270</div>
+
+                <div className="font-medium md:text-xl">
+                    {
+                        "The Nike Air Max 270 is the best shoe  to ever come out this year"
+                    }
+                </div>
+                <div className="text-3xl font-extrabold md:text-6xl">
+                    $100
+                </div>
+                <div className="space-x-10">
+                    <button className="h-14 w-1/2 bg-black text-white hover:bg-gray-900 active:bg-gray-500">
+                        Add to bag
+                    </button>
+                    <a href="#" className="text-lg font-bold underline underline-offset-4">
+                        View Details
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    )
+}
--- tailwindcss-tutorial/src/app/page.tsx
+++ tailwindcss-tutorial/src/app/page.tsx
@@ -1,10 +1,11 @@
 import {Nav} from "@/app/components/Nav";
+import {ShoeDetails} from "@/app/components/ShoeDetails";
 
 const MainPage = () => {
     return (
-        <div className="p-2">
+        <div className="px-10 py-5 md:px-20">
             <Nav/>
-
+            <ShoeDetails/>
         </div>
     )
 }
