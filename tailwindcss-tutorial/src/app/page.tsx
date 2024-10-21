d50cf9bcea753b9434e8a3c8c2503e8086764941
YifanXu1999 <yifanxu1999@gmail.com>
2024-10-20 21:54:53 -0400

    typography

--- tailwindcss-tutorial/src/app/page.tsx
+++ tailwindcss-tutorial/src/app/page.tsx
@@ -1,12 +1,14 @@
 export default function Home() {
   return (
       <div>
-          <h1 className="text-green-500">
-              Hello world!
-          </h1>
-          <h1 className='text-yellow-600'>
-              nice
-          </h1>
+          <h1 className='text-7xl'>Title 1</h1>
+          <h2>Title 2</h2>
+          <h3>Title 3</h3>
+          <p className='text-base'>A regular paragraph</p>
+          <p className='text-sm'>A description paragraph</p>
+          <p className='text-xs note'> A little note</p>
+
+
       </div>
 
   )
