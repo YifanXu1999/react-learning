04744c367238a66ad252f9a6648f96a773553544
YifanXu1999 <yifanxu1999@gmail.com>
2024-10-22 10:45:46 -0400

    build single card

--- tailwindcss-tutorial/src/app/page.tsx
+++ tailwindcss-tutorial/src/app/page.tsx
@@ -1,11 +1,13 @@
 import {Nav} from "@/app/components/Nav";
 import {ShoeDetails} from "@/app/components/ShoeDetails";
+import {Card, SHOE_LIST} from "@/app/components/Card";
 
 const MainPage = () => {
     return (
         <div className="animate-fadeIn px-10 py-5 md:px-20">
-            <Nav/>
-            <ShoeDetails/>
+            {/*<Nav/>*/}
+            {/*<ShoeDetails/>*/}
+            <Card item={SHOE_LIST[0]}></Card>
         </div>
     )
 }
