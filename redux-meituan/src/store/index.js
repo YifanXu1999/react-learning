964c974d27dd1be294067b2d8060668768855302
YifanXu1999 <yifanxu1999@gmail.com>
2024-10-24 21:56:35 -0400

    init nextjs

--- redux-meituan/src/store/index.js
+++ redux-meituan/src/store/index.js
@@ -1,1 +1,11 @@
+import {configureStore} from "@reduxjs/toolkit";
+import takeawayReducer from "./modules/takeaway";
+
+  const store = configureStore({
+    reducer: {
+        takeaway: takeawayReducer
+    }
+})
+
+export default store
 
--- redux-meituan/src/store/modules/takeaway.js
+++ redux-meituan/src/store/modules/takeaway.js
@@ -1,15 +1,43 @@
 import {createSlice} from "@reduxjs/toolkit";
+import axios from "axios";
+import count from "../../components/Count";
 
-const takewayStore = createSlice(
-    {
-        name: 'takeaway',
-        initialState: {
-            takeawayList: []
+const takewayStore = createSlice({
+    name: 'foods', initialState: {
+        foodList: [],
+        activeIndex: 0,
+        cartList: []
+    }, reducers: {
+        setFoodList: (state, action) => {
+            state.foodList = action.payload
         },
-        reducers: {
-            setTakeawayList: (state, action) => {
-                state.takeawayList = action.payload
+        changeActiveIndex: (state, action) => {
+            state.activeIndex = action.payload
+        },
+        addCart(state, action) {
+            console.log('addCart', action.payload)
+            const item = state.cartList.find(item => item.id === action.payload.id)
+            if(item) {
+                console.log('item', item)
+                item.count++
+                return
             }
+            state.cartList.push({...action.payload, count:0})
+
         }
     }
-)
+})
+
+
+const {setFoodList,changeActiveIndex,addCart} = takewayStore.actions
+
+const fetchFoodList = () => {
+    return async (dispatch) => {
+        const res = await axios.get('http://localhost:3004/takeaway')
+        dispatch(setFoodList(res.data))
+    }
+}
+const takeawayReducer = takewayStore.reducer
+
+export {fetchFoodList, changeActiveIndex, addCart}
+export default takeawayReducer
