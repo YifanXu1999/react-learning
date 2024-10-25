3d8fa5455d6067d1ba61f4a8d9b9abeec3f71f04
YifanXu1999 <yifanxu1999@gmail.com>
2024-10-24 22:35:57 -0400

    template vs layout

--- NextJsTutorial/my-app/src/app/dashboard/about/page.tsx
+++ NextJsTutorial/my-app/src/app/dashboard/about/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return (
         <div>
-
+            Settings Content
         </div>
     )
 }
--- NextJsTutorial/my-app/src/app/dashboard/layout.tsx
+++ NextJsTutorial/my-app/src/app/dashboard/layout.tsx
@@ -1,3 +1,7 @@
+"use client";
+import Link from "next/link";
+import {useState} from "react";
+
 
 export default function DashboardLayout(
     {
@@ -5,9 +9,15 @@ export default function DashboardLayout(
     }: Readonly<{
         children: React.ReactNode;
     }>) {
+    const [count, setCount] = useState(0)
     return (
-        <div>
-            <h2>I am root Dashboard Layout</h2>
+        <div className="border-2 border-dashed border-black p-4 w-1/2 mx-auto mt-10">
+            <div className="flex justify-between font-bold text-lg mb-4 text-purple-500">
+                <Link href="/dashboard/about">About </Link>
+                <Link href="/dashboard/settings">Settings </Link>
+            </div>
+            <h2>Dashboard Layout {count}</h2>
+            <button className="bg-black text-white border rounded-2xl p-2" onClick={()=>setCount(count+1)}>Increment</button>
             {children}
         </div>
     );
